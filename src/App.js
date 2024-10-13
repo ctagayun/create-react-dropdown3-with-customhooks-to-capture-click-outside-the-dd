3c8587945b32f2e1c33e9960713faf1da9ab3f05
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-13 11:42:42 -0500

    save

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ const style = {
 };
 
 const App = () => {
+  //Initialize the state to zero
   const [count, setCount] = React.useState(0);
+
+  //event handler for the dropdown click event
+  const handleClick = () => {
+    setCount((state) => count + 1);
+  };
 
   const handleMenuOne = () => {
     console.log('clicked one');
@@ -42,11 +48,10 @@ const App = () => {
     setCount(0);
   };
 
-  const handleClick = () => {
-    setCount((state) => state + 1);
-  };
-
   //Now let use the custom hook called "useOutsideClick" from "./hooks/useOutsideClick";
+  // Now the custom hook can be used the following way in 
+ //our React component: pass the event handler as callback 
+ // function to the hook -- which executes whenever the document gets clicked.
   const ref = useOutsideClick(handleClickOutside);
 
   return (
@@ -62,7 +67,7 @@ const App = () => {
         <Dropdown  
           //Now we will use the "ref" returned by "useOutsideClick" 
           //be considered as an outside click.
-          trigger={<button ref={ref} onClick={handleClick} >Show Dropdown</button>}
+          trigger={<button ref={ref} onClick={handleClick} >MenuDropdown</button>}
          
           //populate the "menu" prop
           menu={[
@@ -91,48 +96,33 @@ const App = () => {
 //React's cloneElement API:
 
 const Dropdown = ({ trigger, menu }) => {
-  //Code to handle outside click
+  
   const [count, setCount] = React.useState(0);
-
-  const handleClickOutside = () => {
-    setCount(0);
- };
-
-  //Our custom hook can be used the following way in our React component:
-  //pass the event handler as callback function to the hook -- which executes 
-  //whenever the document gets clicked. In addition, use the reference by our
-  //custom hook (here: ref) and assign it to the li HTML element. 
-  //Pass handleClickOutside as callback handler.
-  const ref = useOutsideClick(handleClickOutside);
-
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
     setOpen(!open);
   };
-
+ 
   return (
-     
     <div className="dropdown">
        {React.cloneElement(trigger, {  //trigger is what opens the dropdown
         onClick: handleOpen,
       })}   
      
-      {/* React's cloneElement API allows us to attach props to the passed trigger element (here: opening/closing the dropdown, because it toggles the open state within the dropdown component). */}
+      {/* React's cloneElement API allows us to attach props to the passed trigger 
+        element (here: opening/closing the dropdown, because it toggles the open 
+        state within the dropdown component). */}
       {open ? (
         <ul className="menu">
           {menu.map((menuItem, index) => (
-            <li key={index} className="menu-item" ref={ref}>
+            <li key={index} className="menu-item">
                {/* replicate the menuitem in every iteration */}
                {React.cloneElement(menuItem, {
                 onClick: () => {
-                  // Furthermore, the high-level API allows us to 
-                  //close the dropdown once a menu item in a native
-                  //dropdown is clicked while still preserving its 
-                  //implementation (here: menuItem.props.onClick).
                   menuItem.props.onClick(); //high-level API that allows us to close the dropdown once a 
                                             //menu item in a dropdown is clicked while still preserving 
-                                            //its native implementation 
-                  setOpen(false); //close the dropdwn
+                                            //its native implementation (here: menuItem.props.onClick). 
+                  setOpen(false);           //close the dropdwn
                   setCount((state) => state + 1);  //increments the state count. the number of times the list item was clicked
                 },
               })}
--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,24 +1,57 @@
 import * as React from "react";
 
 /*    
-  Now the custom hook can be used the following way in 
+   Now the custom hook can be used the following way in 
 our React component: pass the event handler as callback 
 function to the hook -- which executes whenever the document 
 gets clicked. In addition, use the returned reference (here: ref) 
 and assign it to the button HTML element: 
-  See hacker9-stories-react-side-effects-useEffects-9 to on how to 
-use React.useEffect
 
+  See hacker9-stories-react-side-effects-useEffects-9 to on how to use React.useEffect
   See hacker10-stories-on-react-custom-hooks on how to create custom hooks
   See hacker14-stories-react-imperative-react on how to use React.useRef
 */
 const useOutsideClick = (callback) => {
 
     console.log("Custom hook useOutsideClick fired");
-    //custom hook initiates a React.useRef (tps://www.robinwieruch.de/react-ref/) 
 
+    //What is a userRef? The hook initiates a React.useRef (https://www.robinwieruch.de/react-ref/) 
     const ref = React.useRef(); 
-    
+
+    //What does useEffect do? by using this hook you tell React that 
+    //your component needs to do something after a render.
+
+    //In this scenario the purpose of useEffect Hook here is to assign (and remove)  
+    //an event listener(here: click event) on document level. After all, whenever the d
+    //document gets clicked, the handler and the callback function you passed will run.
+    React.useEffect(() => {
+      const handleClick = (event) => {
+
+        //What we want to accomplish: Execute the callback function only when 
+        //anything outside of the passed ref (representing the dropdown here) is clicked, 
+        //not when the ref itself (or its content) gets clicked:
+
+        //The reference assigned to 
+        //the button is the border between triggering the button's event handler and the 
+        //document's event handler. Everything clicked that's outside of the reference will 
+        //be considered as an outside click.
+        if (ref.current && !ref.current.contains(event.target)) {
+          callback();
+        }
+      };
+  
+      document.addEventListener('click', handleClick);
+  
+      return () => {
+        document.removeEventListener('click', handleClick);
+      };
+    }, []);
+  
+    return ref;
+  };
+  
+  export default useOutsideClick;
+
     //What does useEffect do? by using this hook you tell React that 
     //your component needs to do something after a render.
     //useEffect is a Hook, so you can only call it at the top level 
@@ -39,35 +72,3 @@ const useOutsideClick = (callback) => {
      //the function is called every time the searchTerm changes 
      //(e.g. when a user types into the HTML input field).    
     //https://www.robinwieruch.de/react-useeffect-hook/
-
-    //The purpose of useEffect Hook here is to assign (and remove)  
-    //an event listener(here: click event) on document level. After all, 
-    //whenever the document gets clicked, the handler and the  
-    //callback function you passed will run.
-    React.useEffect(() => {
-      const handleClick = (event) => {
-
-        //What we want to accomplish: Execute the callback function only when 
-        //anything outside of the passed ref (representing the button here) is clicked, 
-        //not when the ref itself (or its content) gets clicked:
-
-        //The reference assigned to 
-        //the button is the border between triggering the button's event handler and the 
-        //document's event handler. Everything clicked that's outside of the reference will 
-        //be considered as an outside click.
-        if (ref.current && !ref.current.contains(event.target)) {
-          callback();
-        }
-      };
-  
-      document.addEventListener('click', handleClick);
-  
-      return () => {
-        document.removeEventListener('click', handleClick);
-      };
-    }, [ref]);
-  
-    return ref;
-  };
-  
-  export default useOutsideClick;
